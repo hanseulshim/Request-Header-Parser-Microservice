6198f3a1f0ca05bc9a52fadf3debf84fbbbb743b
Hanseul Shim <hanseulshim@gmail.com>
2017-05-18 22:47:39 +0000

    add user information

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello dosijf!')
+app.get('/:api/:whoami', function (req, res) {
+  var ipAddress = req.headers['x-forwarded-for']
+  var language = req.headers["accept-language"].split(',')[0]
+  var software = req.headers['user-agent'].match(/\((.+?)\)/)[1]
+  var data = {
+      ipAddress: ipAddress,
+      language: language,
+      software: software
+  }
+  res.send(data)
+})
+
+app.get('/', function(req,res) {
+    res.send("Please go to /api/whoami to see your browser information")
 })
 
 app.listen(8080, function () {
